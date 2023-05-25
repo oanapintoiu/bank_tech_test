e82a66a876e1dd821a555d5fa9c9cdfca94c748e
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-05-25 15:37:03 +0100

    passed second withdrawal feature test, 100% code coverage

--- tests/bankAccount.test.js
+++ tests/bankAccount.test.js
@@ -26,6 +26,13 @@ describe("Bank Account", () => {
         expect(account.balance).toBe(1000);
       });
 
+      it("should decrease the balance from 2000 to 1000", () => {
+        account.deposit(2000);
+        account.withdraw(1000);
+        account.withdraw(500);
+        expect(account.balance).toBe(500);
+      });
+
     });
   });
 });
